e2087e68f54637495dd17603b8192f3ae9b5e04a
akashzone <akashnadar.dev@gmail.com>
2026-01-14 21:54:39 +0530

    fix: remove duplicate variable declaration in adminRoutes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const Admin = require('../models/Admin');
-const Admin = require('../models/Admin');
 const Order = require('../models/Order');
 const MonthlySlot = require('../models/MonthlySlot');
 const getCurrentMonth = require('../utils/getCurrentMonth');
